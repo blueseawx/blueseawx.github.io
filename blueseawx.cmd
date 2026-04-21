2e3e59961a6335674e3d50e568b9592fa95c65ab
blueseawx <71841013@qq.com>
2026-04-21 15:07:23 +0800

    Add files via upload

---
+++ blueseawx.cmd
@@ -0,0 +1,24 @@
+@echo off
+chcp 65001
+title 吉米Jimmy | 技术能力简介
+cls
+echo.
+echo                    ▄▄▄▄▄▄▄▄▄▄▄▄
+echo                 ▄???????????????▄
+echo               ▄?                ?▄
+echo              ?                    ?
+echo        ?? 前沿技术 · 吉米 Jimmy ??
+echo ========================================
+echo  name：吉米  Jimmy
+echo  University：上海交通大学
+echo ========================================
+echo  专注于：
+echo  ? Java 安卓应用开发
+echo  ? Python 工具脚本开发
+echo  ? Linux 日常运维与环境管理
+echo  ？数据库管理与优化
+echo  ? 自动化脚本编写、批量任务处理
+echo ========================================
+echo  擅长：轻量化脚本落地、高效自动化实现
+echo ========================================
+pause >nul
